6709bd3c94563bdb649dcb3f608554782fc159bf
Mupersega <cambodiam@hotmail.com>
2021-01-25 12:26:40 +1000

    updated loop problems but they still exist

--- __main__.py
+++ __main__.py
@@ -7,11 +7,8 @@ import skrappa
 if __name__ == '__main__':
 
 	update_all = 1
-	# ____PULLA____ #
 	pull = 0
-	# ____FLIPPA____ #
 	flip = 0
-	# ___SKRAPPA___ #
 	scrap = 1
 
 	# pull info
@@ -23,33 +20,32 @@ if __name__ == '__main__':
 	buy_from = 'hub'
 	sell_to_region = 'the forge'
 	sell_to = 'hub'
-	isk_threshold = 1000
+	isk_threshold = 5000
 	percent_return = 10
 	ores_only = False
 
 	all_regions = ['heimatar', 'domain', 'the forge', 'metropolis', 'sinq laison']
-
+	# ____PULLA____ #
 	if pull:
 		if update_all:
 			# noinspection PyRedeclaration
 			pull_regions = all_regions
 			new_pulla = pulla.Pulla(pull_regions)
 			new_pulla.main()
-
+	# ___SKRAPPA___ #
 	if scrap:
 		scrap_regions = [buy_from_region, sell_to_region]
 		if update_all:
-			new_pulla = pulla.Pulla(scrap_regions)
+			new_pulla = pulla.Pulla(set(scrap_regions))
 			new_pulla.main()
 		new_scrap = skrappa.Skrappa(
 			buy_from_region, buy_from, sell_to_region, sell_to, ores_only, percent_return, isk_threshold)
 		new_scrap.main()
-
+	# ____FLIPPA____ #
 	if flip:
 		if update_all:
 			flip_regions = all_regions
-		else:
-			new_pulla = pulla.Pulla(flip_regions)
-			new_pulla.main()
+		new_pulla = pulla.Pulla(flip_regions)
+		new_pulla.main()
 		new_flip = flippa.Flippa(flip_regions)
 		new_flip.main()
